502f8a3ba936e0cef9c100077cdccb177165b09a
Sergey Filatov <filatov300@gmail.com>
2023-02-17 17:17:05 +0200

    add counter basic

---
+++ src/components/Theme/Theme.jsx
@@ -0,0 +1,52 @@
+export const theme = {
+	colors: {
+		backgroundColor: ' rgba(224, 255, 255, 1)',
+		black: 'rgba(17, 17, 17, 1)',
+		white: 'rgba(255, 255, 255, 1)',
+		text: 'rgba(17, 17, 17, 1)',
+		counter:{
+			backgroundColor: 'rgba(230, 230, 250, 1)',
+			titleColor: 'rgba(85, 101, 129,1)',
+			titleValueColor: 'rgba(17, 17, 17, 1)',
+		},
+		},
+	fontSizes: {
+		a: '12px',
+		b: '14px',
+		c: '16px',
+		d: '18px',
+		i: '20px',
+		f: '24px',
+		g: '28px',
+		h: '32px',
+		j: '48px',
+	 },
+	 fontWeights: {
+		normal: 400,
+		preBold: 500,
+		bold: 700,
+	 },
+	 lineHeights: {
+		a: 1.125,
+		b: 1.5,
+		c: 1.36,
+		d: 1.375,
+		e: 1.35,
+		f: 1.2,
+	 },
+	 letterSpacing: {
+		a: '-0.01em',
+		b: '0.04em',
+	 },
+	 borders: {
+		none: 'none',
+		normal: '1px solid',
+	 },
+	 radii: {
+		none: '0',
+		sm: '8px',
+		md: '20px',
+		lg: '40px',
+		round: '50%',
+	 },
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'components/Theme/Theme';
+import { GlobalStyles } from 'utility/GlobalStyles';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+	<ThemeProvider theme={theme}>
+		<GlobalStyles/>
     <App />
-  </React.StrictMode>
+	 </ThemeProvider>
+   </React.StrictMode>
 );
