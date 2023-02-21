2406322a19676a93de73397669d2ae9e1a50a1b3
Sergey Filatov <filatov300@gmail.com>
2023-02-21 17:22:54 +0200

    modal fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import Counter from "./Counter/Counter";
-import Modal from "./Counter/Modal/Modal";
+import Modal from "./Modal/Modal";
+import { StyledModalWrapper } from "./Modal/Modal.styled";
+import ModalBtnOpen from "./Modal/ModalBtn/ModalBtnOpen";
 
 export const App = () => {
+
+	const[open, setOpen] = useState(false);
+
+	
+	const openModal =() => {
+		setOpen(true)
+	};
+
+	const closeModal =() => {
+		setOpen(false)
+	};
+
   return (
   <>
   <Counter/>
-  <Modal/>
-  
+  <StyledModalWrapper>
+  <ModalBtnOpen btnTitle='open modal' onClick={openModal}/>
+  { open && <Modal  onClose= {closeModal}>
+	{/* <h1>Hello</h1> */}
+	<img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+	</Modal>}
+	</StyledModalWrapper>
   </>
   );
 };
